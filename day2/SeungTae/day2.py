7fdb0b59797890d9bf166b3389b3cb97ddfd3bf8
LST <dev.st1027@gmail.com>
2023-03-17 13:27:50 +0900

    <FEAT>: Day2 이승태 풀이

---
+++ day2/SeungTae/day2.py
@@ -0,0 +1,18 @@
+global s
+s = "12:13:50PM"
+
+def timeConversion(s):
+    if s[-2:] == "PM":
+        hh = int(s[0:2])
+        s = s[2:-2]
+        if(hh==12):
+            s = "00" + s
+            print(s)
+            return
+        elif(1<=hh and hh<12):
+            hh+=12
+        else: return
+        s = str(hh)+ s
+    print(s)
+
+timeConversion(s)
