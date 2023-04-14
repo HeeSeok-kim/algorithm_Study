c50b8b8239a5c847f8b2f2a00c66cb29a5bf1f41
LST <dev.st1027@gmail.com>
2023-04-14 23:26:28 +0900

    <FEAT>: 13일차 진료 순서 정하기 풀이 2

--- day13/SeungTae/진료 순서 정하기.py
+++ day13/SeungTae/진료 순서 정하기.py
@@ -10,3 +10,17 @@ def solution(emergency):
 
 
 solution([3, 76, 24])
+
+"""
+# for문 하나 덜 쓰는 방법
+
+def solution(emergency):
+    #for i in range(len(emergency)):
+        #answer.append(len(emergency))
+    answer = len(emergency)*[len(emergency)]
+    for i in range(len(emergency)):
+        for j in range(len(emergency)):
+            if(emergency[i]>emergency[j]):
+                answer[i] -= 1
+    return answer
+"""
